1582dfac5d080e3def3402c340959ba50a490f3e
Page Piccinini <page.piccinini@gmail.com>
2016-05-20 11:52:36 +0200

    Add comments to statistic part 2 script.

--- scripts/rcourse_lesson6_pt2_statistics.R
+++ scripts/rcourse_lesson6_pt2_statistics.R
@@ -9,25 +9,28 @@ library(lme4)
 ## ORGANIZE DATA ####
 # Accuracy data
 data_accuracy_stats = data_accuracy_clean %>%
+  # Add contrast coding for experimental variables
   mutate(congruency_contrast = ifelse(congruency == "con", -0.5, 0.5)) %>%
   mutate(half_contrast = ifelse(half == "first", -0.5, 0.5))
 
+# Double check contrast coding done correctly
 xtabs(~congruency+congruency_contrast, data_accuracy_stats)
 xtabs(~half+half_contrast, data_accuracy_stats)
 
-
 # RT data
 data_rt_stats = data_rt_clean %>%
+  # Add contrast coding for experimental variables
   mutate(congruency_contrast = ifelse(congruency == "con", -0.5, 0.5)) %>%
   mutate(half_contrast = ifelse(half == "first", -0.5, 0.5))
 
+# Double check contrast coding done correctly
 xtabs(~congruency+congruency_contrast, data_rt_stats)
 xtabs(~half+half_contrast, data_rt_stats)
 
 # Check within or between variables
-xtabs(~subject_id+congruency+half, data_accuracy_stats)
-xtabs(~item+congruency+half, data_accuracy_stats)
-xtabs(~item+half, data_accuracy_stats)
+xtabs(~subject_id+congruency_contrast+half_contrast, data_accuracy_stats)
+xtabs(~item+congruency_contrast+half_contrast, data_accuracy_stats)
+xtabs(~item+half_contrast, data_accuracy_stats)
 
 
 ## BUILD MODELS FOR ACCURACY ANALYSIS ####
@@ -50,6 +53,10 @@ accuracy.glmer = glmer(accuracy ~ congruency_contrast * half_contrast +
 accuracy.glmer_sum = summary(accuracy.glmer)
 accuracy.glmer_sum
 
+# Get coefficients and save
+accuracy.glmer_coef = coef(accuracy.glmer)
+accuracy.glmer_coef
+
 # Test for effect of congruency
 accuracy_congruency.glmer = glmer(accuracy ~ congruency_contrast * half_contrast - congruency_contrast +
                                     (1|subject_id) +
@@ -58,6 +65,7 @@ accuracy_congruency.glmer = glmer(accuracy ~ congruency_contrast * half_contrast
                                     (0+half_contrast|item), family = "binomial",
                                   data = data_accuracy_stats)
 
+# Compare full model to reduced model without congruency
 accuracy_congruency.anova = anova(accuracy.glmer, accuracy_congruency.glmer)
 accuracy_congruency.anova
 
@@ -69,6 +77,7 @@ accuracy_half.glmer = glmer(accuracy ~ congruency_contrast * half_contrast - hal
                               (0+half_contrast|item), family = "binomial",
                             data = data_accuracy_stats)
 
+# Compare full model to reduced model without experiment half
 accuracy_half.anova = anova(accuracy.glmer, accuracy_half.glmer)
 accuracy_half.anova
 
@@ -80,11 +89,12 @@ accuracy_congruencyxhalf.glmer = glmer(accuracy ~ congruency_contrast * half_con
                                          (0+half_contrast|item), family = "binomial",
                                        data = data_accuracy_stats)
 
+# Compare full model to reduced model interaction of congruency x experiment half
 accuracy_congruencyxhalf.anova = anova(accuracy.glmer, accuracy_congruencyxhalf.glmer)
 accuracy_congruencyxhalf.anova
 
 
-## BUILD MODELS FOR RT ANALYSIS ####
+## BUILD MODELS FOR REACTION TIME ANALYSIS ####
 # Full model
 # First model failed to converge
 rt_log10.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast +
@@ -99,8 +109,13 @@ rt_log10.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast +
                          (0+half_contrast|item), REML = F,
                        data = data_rt_stats)
 
+# Summarise model and save
 rt_log10.lmer_sum = summary(rt_log10.lmer)
 rt_log10.lmer_sum
+
+# Get coefficients and save
+rt_log10.lmer_coef = coef(rt_log10.lmer)
+rt_log10.lmer_coef
 
 # Test for effect of congruency
 rt_log10_congruency.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast - congruency_contrast +
@@ -109,6 +124,7 @@ rt_log10_congruency.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast -
                                   (0+half_contrast|item), REML = F,
                                 data = data_rt_stats)
 
+# Compare full model to reduced model without congruency
 rt_log10_congruency.anova = anova(rt_log10.lmer, rt_log10_congruency.lmer)
 rt_log10_congruency.anova
 
@@ -119,6 +135,7 @@ rt_log10_half.lmer = lmer(rt_log10 ~ congruency_contrast * half_contrast - half_
                             (0+half_contrast|item), REML = F,
                           data = data_rt_stats)
 
+# Compare full model to reduced model without experiment half
 rt_log10_half.anova = anova(rt_log10.lmer, rt_log10_half.lmer)
 rt_log10_half.anova
 
@@ -129,6 +146,7 @@ rt_log10_congruencyxhalf.lmer = lmer(rt_log10 ~ congruency_contrast * half_contr
                                        (0+half_contrast|item), REML = F,
                                      data = data_rt_stats)
 
+# Compare full model to reduced model interaction of congruency x experiment half
 rt_log10_congruencyxhalf.anova = anova(rt_log10.lmer, rt_log10_congruencyxhalf.lmer)
 rt_log10_congruencyxhalf.anova
 
